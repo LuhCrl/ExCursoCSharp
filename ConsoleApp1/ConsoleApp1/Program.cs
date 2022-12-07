62a2fe624cdab22fef706888dbd5cacdc0555f59
Lucas Clerio <lucasclerio96@gmail.com>
2022-12-07 14:50:19 -0300

    Curso HTML/CSS

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Globalization;
+
+string nome = "Nicoli";
+int idade = 22;
+double tamanho = 1.65;
+
+Console.WriteLine($"O nome dela é {nome}, tem {idade} anos e mede {tamanho.ToString("F2", CultureInfo.InvariantCulture)}");
