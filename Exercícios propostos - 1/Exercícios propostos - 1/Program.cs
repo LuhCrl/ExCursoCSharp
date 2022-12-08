0dbef9e6273f35338b6c65386b656d48e67e6bb7
Lucas Clerio <lucasclerio96@gmail.com>
2022-12-08 10:00:20 -0300

    CursoCSharp

---
+++ Exercícios propostos - 1/Exercícios propostos - 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+int M, N;
+int[,] A;
+
+string[] s = Console.ReadLine().Split(' ');
+M = int.Parse(s[0]);
+N = int.Parse(s[1]);
+A = new int[M, N];
+
+for (int i = 0; i < M; i++)
+{
+    string[] r = Console.ReadLine().Split(' ');
+    for(int j = 0; j < N; j++)
+    {
+        A[i,j] = int.Parse(r[j]);
+    }
+}
+
+for(int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (A[i, j] < 0)
+        {
+            Console.WriteLine("Valores Negativos: ");
+            Console.WriteLine(A[i, j]);
+        }
+    }
+}
+
+
+
