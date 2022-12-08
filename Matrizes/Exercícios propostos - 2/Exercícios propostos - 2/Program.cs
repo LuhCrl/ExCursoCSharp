5cd374139e996d651c4b0e5f5bdfc306f8c488d9
Lucas Clerio <lucasclerio96@gmail.com>
2022-12-08 10:55:38 -0300

    Exercícios propostos

---
+++ Matrizes/Exercícios propostos - 2/Exercícios propostos - 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+int N;
+int[,] mat;
+int[] soma;
+
+N = int.Parse(Console.ReadLine());
+mat = new int[N,N];
+soma = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    string[] s = Console.ReadLine().Split();
+    for(int j = 0; j < N; j++)
+    {
+        mat[i, j] = int.Parse(s[j]);
+        soma[i] += mat[i, j];
+    }
+
+}
+for(int i = 0; i < N; i++)
+{
+    Console.WriteLine(soma[i]);
+}
---
+++ Matrizes/Exercícios propostos - 3/Exercícios propostos - 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+int N;
+int[,] met;
+int[] maior;
+
+N = int.Parse(Console.ReadLine());
+met = new int[N, N];
+maior = new int[N];
+
+for(int i = 0; i < N; i++)
+{
+    string[] s = Console.ReadLine().Split(' ');
+    for(int j =0; j < N; j++)
+    {
+        met[i, j] = int.Parse(s[j]);
+    }
+}
+for(int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (met[i, j] > maior[i])
+        {
+            maior[i] = met[i, j];
+        }
+    }
+    Console.WriteLine(maior[i]);
+}
