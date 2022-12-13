f54e2173296fc0303db6f1d8d41836432b761c59
Lucas Clerio <lucasclerio96@gmail.com>
2022-12-13 20:44:13 -0300

    Curso

---
+++ Matrizes/Exercícios propostos - 4/Exercícios propostos - 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+int N;
+int soma = 0;
+int[,] mat;
+
+N = int.Parse(Console.ReadLine());
+mat = new int[N, N];
+
+for (int i = 0; i < N; i++)
+{
+    string[] s = Console.ReadLine().Split(' ');
+    for(int j = 0; j < N; j++)
+    {
+        mat[i, j] = int.Parse(s[j]);
+    }
+}
+
+for(int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (i == 0 && j >= 1 )
+        {
+            soma += mat[i, j];
+            
+        }
+    }
+}
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (i == 1 && j == 2)
+        {
+            soma += mat[i, j];
+
+        }
+    }
+}
+
+
+Console.WriteLine(soma);
